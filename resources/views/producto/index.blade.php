10ab6c7b4f815420485451286b64d9ad0f1c06fc
Luis <luisruizpena03@gmail.com>
2025-04-20 14:29:34 -0400

    arreglando responsive en medium displays en las tarjetas de productos

--- resources/views/producto/index.blade.php
+++ resources/views/producto/index.blade.php
@@ -1,40 +1,38 @@
 <x-admin-layout>
     <x-search-bar ruta="producto"></x-search-bar>
-    <div class="mt-3 grid grid-cols-1 gap-4 text-wrap md:grid-cols-4">
+    <div class="mt-3 grid grid-cols-1 place-items-center gap-4 text-wrap md:grid-cols-2 lg:grid-cols-4">
         @foreach ($productos as $producto)
-            <div>
-                <div class="flex h-96 w-full flex-col rounded-lg bg-white text-center shadow">
-                    <div class="size-full h-32 overflow-hidden">
-                        <img src="{{ asset($producto->imagen) }}" alt="{{ $producto->nombre }}" class="rounded-t-lg">
+            <div class="flex h-96 w-fit flex-col rounded-lg bg-white text-center shadow">
+                <div class="size-full h-32 overflow-hidden">
+                    <img src="{{ asset($producto->imagen) }}" alt="{{ $producto->nombre }}" class="rounded-t-lg">
+                </div>
+                <div class="h-64 space-y-2 p-4">
+                    <div
+                        class="rounded-lg bg-yellow-200 bg-opacity-50 p-1.5 text-lg font-medium uppercase tracking-wider text-yellow-800">
+                        {{ $producto->nombre }}
                     </div>
-                    <div class="h-64 space-y-2 p-4">
-                        <div
-                            class="rounded-lg bg-yellow-200 bg-opacity-50 p-1.5 text-lg font-medium uppercase tracking-wider text-yellow-800">
-                            {{ $producto->nombre }}
-                        </div>
-                        <div class="text-base text-gray-400">
-                            {{ $producto->categoria->nombre }}
-                        </div>
-                        <div class="h-[7.5rem] text-wrap text-sm text-gray-700">
-                            {{ $producto->descripcion }}
-                        </div>
+                    <div class="text-base text-gray-400">
+                        {{ $producto->categoria->nombre }}
+                    </div>
+                    <div class="h-[7.5rem] text-wrap text-sm text-gray-700">
+                        {{ $producto->descripcion }}
+                    </div>
 
-                        <div class="flex place-content-between">
-                            <div class="text-sm font-medium text-black">
-                                {{ $producto->precio }}$
-                            </div>
-                            <div>
-                                <a class="inline-block" href="{{ route('producto.edit', $producto) }}"><img
-                                        class="h-4" src="/images/nav-icons/editar.svg" alt=""></a>
-                                <form class="inline-block" action="{{ route('producto.destroy', $producto) }}"
-                                    method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button>
-                                        <img class="h-4" src="/images/nav-icons/borrar.svg" alt="">
-                                    </button>
-                                </form>
-                            </div>
+                    <div class="flex place-content-between">
+                        <div class="text-sm font-medium text-black">
+                            {{ $producto->precio }}$
+                        </div>
+                        <div>
+                            <a class="inline-block" href="{{ route('producto.edit', $producto) }}"><img class="h-4"
+                                    src="/images/nav-icons/editar.svg" alt=""></a>
+                            <form class="inline-block" action="{{ route('producto.destroy', $producto) }}"
+                                method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button>
+                                    <img class="h-4" src="/images/nav-icons/borrar.svg" alt="">
+                                </button>
+                            </form>
                         </div>
                     </div>
                 </div>
